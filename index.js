31d9164af0e3ef32cad6755fd10a5c7eb93fff6a
Akanksha <akankshamaurya211@gmail.com>
2026-07-10 16:01:40 +0530

    change origin index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "https://akankshagroceries-zara12.vercel.app",
+    origin: "https://akankshagroceries.vercel.app",
     credentials: true
 }));
 
